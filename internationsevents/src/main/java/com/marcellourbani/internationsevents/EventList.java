2e9f655bd2049865b4e06f7f2d3c604eb3d66341
MARCELLO <marcello.urbani@gmail.com>
2014-02-01 00:48:57 +0000

    icons in list

--- internationsevents/src/main/java/com/marcellourbani/internationsevents/EventList.java
+++ internationsevents/src/main/java/com/marcellourbani/internationsevents/EventList.java
@@ -1,7 +1,8 @@
 package com.marcellourbani.internationsevents;
 
+import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.support.v4.app.ListFragment;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -16,66 +17,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import com.squareup.picasso.Picasso;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 
-public class EventList extends ActionBarActivity implements ActionBar.OnNavigationListener {
-
-    /**
-     * The serialization (saved instance state) Bundle key representing the
-     * current dropdown position.
-     */
-    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+public class EventList extends ActionBarActivity {
+    protected static InternationsBot mIbot;
+    private static EventsFragment mFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mIbot = new InternationsBot(PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_event_list);
-
-        // Set up the action bar to show a dropdown list.
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-
-        // Set up the dropdown list navigation in the action bar.
-        actionBar.setListNavigationCallbacks(
-                // Specify a SpinnerAdapter to populate the dropdown list.
-                new ArrayAdapter<String>(
-                        actionBar.getThemedContext(),
-                        android.R.layout.simple_list_item_1,
-                        android.R.id.text1,
-                        new String[]{
-                                getString(R.string.title_future),
-                                getString(R.string.title_subscribed),
-                        }),
-                this);
-    }
-
-    @Override
-    public void onRestoreInstanceState( Bundle savedInstanceState) {
-        // Restore the previously serialized current dropdown position.
-        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-            getSupportActionBar().setSelectedNavigationItem(
-                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+        mFrag = new EventsFragment(mIbot);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, mFrag)
+                    .commit();
         }
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(
+                new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                        if (s.equals("pr_password"))
+                            mFrag.loadevents();
+                    }
+                });
     }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        // Serialize the current dropdown position.
-        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
-                getSupportActionBar().getSelectedNavigationIndex());
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,93 +64,84 @@ public class EventList extends ActionBarActivity implements ActionBar.OnNavigati
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, InPreferences.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(int position, long id) {
-        // When the given dropdown item is selected, show its contents in the
-        // container view.
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, EventsFragment.newInstance(position + 1))
-                .commit();
-        return true;
-    }
-
+    /**
+     * A placeholder fragment containing a simple view.
+     */
     public static class EventsFragment extends ListFragment {
         protected static InternationsBot mIbot;
         private NetWorker mNw;
-        private static final String ARG_SECTION_NUMBER = "section_number";
-        public static EventsFragment newInstance(int sectionNumber) {
-            EventsFragment fragment = new EventsFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+
+        public EventsFragment(InternationsBot bot) {
+            mIbot = bot;
         }
-        public EventsFragment() {
-        }
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            mIbot = new InternationsBot(PreferenceManager.getDefaultSharedPreferences(getActivity()));
-            mNw = new NetWorker();
             View rootView = inflater.inflate(R.layout.fragment_event_list, container, false);
-//            TextView textView = (TextView) rootView.findViewById(R.id.);
-//            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            mNw.execute("1");
+            loadevents();
             return rootView;
         }
-    private class NetWorker extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected void onPostExecute(Boolean o) {
-            super.onPostExecute(o);
-            if(o){
-                ArrayAdapter<InEvent> aa = new ArrayAdapter<InEvent>(getActivity(), R.layout.fragment_event_list, mIbot.getEvents()) {
-                    @Override
-                    public View getView(int position, View convertView, ViewGroup parent) {
-                        View view = convertView;
-                        if(view==null){
-                            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                            view = inflater.inflate(R.layout.event_item,null);
+
+        public void loadevents() {
+            if (mNw == null) mNw = new NetWorker();
+            mNw.execute("");
+        }
+
+        private class NetWorker extends AsyncTask<String, Void, Boolean> {
+            @Override
+            protected void onPostExecute(Boolean o) {
+                super.onPostExecute(o);
+                if (o) {
+                    ArrayAdapter<InEvent> aa = new ArrayAdapter<InEvent>(getActivity(), R.layout.fragment_event_list, mIbot.getEvents()) {
+                        @Override
+                        public View getView(int position, View convertView, ViewGroup parent) {
+                            View view = convertView;
+                            if (view == null) {
+                                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                                view = inflater.inflate(R.layout.event_item, null);
+                            }
+                            InEvent event = getItem(position);
+                            if (event != null) {
+                                DateFormat df = new SimpleDateFormat("dd.MM.yy kk:mm");
+                                TextView title = (TextView) view.findViewById(R.id.eititle);
+                                TextView location = (TextView) view.findViewById(R.id.eilocation);
+                                TextView rsvp = (TextView) view.findViewById(R.id.eirsvp);
+                                TextView start = (TextView) view.findViewById(R.id.eistart);
+                                TextView stop = (TextView) view.findViewById(R.id.eistop);
+                                TextView group = (TextView) view.findViewById(R.id.eigroup);
+                                ImageView icon = (ImageView) view.findViewById(R.id.eiicon);
+                                rsvp.setText(event.mSubscribed ? "Going" : "Not going");
+                                start.setText(event.mStart != null ? df.format(event.mStart.getTime()) : "");
+                                stop.setText(event.mStop != null ? df.format(event.mStop.getTime()) : "");
+                                group.setText(event.mGroup);
+                                Picasso.with(getActivity()).load(event.mIconUrl).into(icon);
+                                title.setText(event.mTitle);
+                                location.setText(event.mLocation);
+                            }
+                            return view;
                         }
-                        InEvent event = getItem(position);
-                        if (event!=null){
-                            DateFormat df = new SimpleDateFormat("dd.MM.yy kk:mm");
-                            TextView title = (TextView) view.findViewById(R.id.eititle);
-                            TextView location = (TextView) view.findViewById(R.id.eilocation);
-                            TextView rsvp = (TextView) view.findViewById(R.id.eirsvp);
-                            TextView start = (TextView) view.findViewById(R.id.eistart);
-                            TextView stop = (TextView) view.findViewById(R.id.eistop);
-                            TextView group = (TextView) view.findViewById(R.id.eigroup);
-                            ImageView icon = (ImageView) view.findViewById(R.id.eiicon);
-                            rsvp.setText(event.mSubscribed?"Going":"Not going");
-                            start.setText(event.mStart != null ? df.format(event.mStart.getTime()) : "");
-                            stop.setText(event.mStop!=null?df.format(event.mStop.getTime()):"");
-                            group.setText("");
-                              //  icon.setImageURI(new URI(event.mIconUrl));
-                            title.setText(event.mTitle);
-                            location.setText(event.mLocation);
-                        }
-                        return view;
-                    }
-                };
-                EventsFragment.this.setListAdapter(aa);
-                aa.notifyDataSetChanged();
+                    };
+                    EventsFragment.this.setListAdapter(aa);
+                    aa.notifyDataSetChanged();
+                } else
+                    startActivity(new Intent(getActivity(), InPreferences.class));
             }
-            else
-                startActivity( new Intent(getActivity(),InPreferences.class));
+
+            @Override
+            protected Boolean doInBackground(String... strings) {
+                if (mIbot.sign())
+                    mIbot.readMyEvents();
+                else
+                    return false;
+                return true;
+            }
         }
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            if (mIbot.sign())
-                mIbot.readMyEvents();
-              else
-                return false;
-            return true;
-        }
-    }
     }
 }
--- internationsevents/src/main/java/com/marcellourbani/internationsevents/InEvent.java
+++ internationsevents/src/main/java/com/marcellourbani/internationsevents/InEvent.java
@@ -6,9 +6,15 @@ import org.jsoup.select.Elements;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InEvent {
-    String mIconUrl, mTitle, mLocation, mEventUrl;
+    private Pattern mActPattern;
+    private Pattern mEventPattern;
+    private String mGroupId;
+    private String mEventId;
+    String mIconUrl, mTitle, mLocation, mEventUrl,mGroup;
     boolean mSubscribed;
     GregorianCalendar mStart, mStop;
 
@@ -24,9 +30,22 @@ public class InEvent {
     InEvent(Element e) {
         try {
             mIconUrl = getAttr(e.select("p.guide-photo img"), 0, "src");
-            Elements tmp = e.select("h3.guide-name a");
-            mTitle = getAttr(tmp, 0, "title");
+            Elements tmp = e.select("div.guide-entry p");
+            if(tmp!=null&&tmp.size()>=2&&!tmp.get(1).hasClass("guide-name"))
+                mGroup = tmp.get(1).text();
+            tmp = e.select("h3.guide-name a");
+            mTitle = tmp!=null&&tmp.size()>0? tmp.get(0).text():"";
             mEventUrl = getAttr(tmp, 0, "href");
+            mActPattern = Pattern.compile("activity-group/([0-9]+)/activity/([0-9]+)");
+            mEventPattern = Pattern.compile("/events/.*[^0-9]([0-9]+)$");
+            Matcher mat = mActPattern.matcher(mEventUrl);
+            if (mat.find()) {
+                mGroupId = mat.group(1);
+                mEventId = mat.group(2);
+            } else {
+                mat = mEventPattern.matcher(mEventUrl);
+                if (mat.find()) mEventId = mat.group(1);
+            }
             mSubscribed = !(getAttr(e.select("span.already-guest img"), 0, "src").equals(""));
             tmp = e.select("td.col_city");
             mLocation = tmp.text();
@@ -37,11 +56,11 @@ public class InEvent {
             tmp = e.select("td.col_datetime p.time");
             String startt = tmp.get(0).text();
             String endt = tmp.size() > 1 ? tmp.get(1).text() : null;
-            if (endt!=null&&endd == null) endd = startd;
+            if (endt != null && endd == null) endd = startd;
             DateFormat df = new SimpleDateFormat("MMM dd,yyyy kk:mm");
             mStart = new GregorianCalendar();
             mStart.setTime(df.parse(startd + " " + startt));
-            if(endd!=null){
+            if (endd != null) {
                 mStop = new GregorianCalendar();
                 mStop.setTime(df.parse(endd + " " + endt));
             }
